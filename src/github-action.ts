4b3198b959a82be0ac2120c05bcf7beb4506cced
Fredrik Anbacken <74957712+anbacken@users.noreply.github.com>
2022-06-08 11:22:05 +0200

    Filter projects with optional filterPath input
    - Filter projects
    - Add test

--- src/github-action.ts
+++ src/github-action.ts
@@ -1,15 +1,41 @@
-import { debug } from '@actions/core';
-import { getMainDiffTarget, isMainBranch } from './lib';
+import { debug, getInput, info, setFailed, setOutput } from '@actions/core';
+import { context } from '@actions/github';
+import { executeCommand, getEventContext, isMainBranch } from './lib';
+import { filterAffectedPackages } from './lib/filter-affected-packages';
 
-export const run = (): void => {
+export const run = async (): Promise<void> => {
   const isMain = isMainBranch();
+  const eventContext = await getEventContext();
 
-  if (isMain) {
-    runForMain();
-  }
+  // TODO: It doesn't handle workflow_dispatch for main branch at the moment since it doesn't expose
+  // github.event.before so we don't know what to compare with.
+  const changedPackages = await getChangedPackages(
+    isMain && context.eventName !== 'workflow_dispatch'
+      ? eventContext.before
+      : 'origin/main',
+  );
+
+  info(`Changed projects: ${JSON.stringify(changedPackages)}`);
+
+  setOutput('changedProjects', changedPackages);
 };
 
-function runForMain() {
-  const mainDiffTarget = getMainDiffTarget();
-  debug(mainDiffTarget);
+async function getChangedPackages(diffTarget: string) {
+  debug(diffTarget);
+
+  if (!diffTarget) {
+    // this only happens when the branch is main and eventContext.before is undefined since
+    // for pull requests the diffTarget is hard coded to 'origin/main'
+    setFailed(`No base sha found`);
+  }
+
+  const { data, error } = await executeCommand(
+    `npx turbo run build --filter=...[${diffTarget}] --dry=json`,
+  );
+
+  if (error) {
+    setFailed(error);
+  }
+
+  return filterAffectedPackages(JSON.parse(data), getInput('filterPath'));
 }
--- src/lib/github-context.ts
+++ src/lib/github-context.ts
@@ -1,15 +1,45 @@
-import { setFailed } from "@actions/core";
-import { context } from "@actions/github"
+import { context } from '@actions/github';
+import { exec } from '@actions/exec';
+import { promises as fsp } from 'fs';
+import { debug, setFailed } from '@actions/core';
 
 export const isMainBranch = (): boolean => {
   return context.ref === 'refs/heads/main';
 };
 
-export const getMainDiffTarget = (): string => {
-  const event = JSON.parse(process.env.GITHUB_EVENT_PATH ?? '{}');
-  if (!event) {
-    setFailed('Could not parse GITHUB_EVENT_PATH');
-  }
+export async function getEventContext() {
+  try {
+    const data = await fsp.readFile(
+      process.env.GITHUB_EVENT_PATH ?? '',
+      'utf-8',
+    );
 
-  return event.before;
-};
+    debug(`event data: ${data}`);
+
+    return JSON.parse(data);
+  } catch (e: any) {
+    setFailed(`Could not read data from GITHUB_EVENT_PATH: ${e.message}`);
+  }
+}
+
+export async function executeCommand(
+  command: string,
+  args?: string[],
+): Promise<{ data: string; error: any }> {
+  let output = '';
+  let error = '';
+
+  const options = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        output += data.toString();
+      },
+      stderr: (data: Buffer) => {
+        error += data.toString();
+      },
+    },
+  };
+
+  await exec(command, args, options);
+  return { data: output, error };
+}
